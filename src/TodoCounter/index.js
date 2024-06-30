0946bfd772c86771ecd7d022c5c4835140a5a309
ricardobertarini <bertarini@gmail.com>
2024-06-30 12:07:04 -0300

    Primer commit

---
+++ src/TodoCounter/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext';
+import './TodoCounter.css';
+
+
+function TodoCounter() {
+  const {
+    completedTodos,
+    totalTodos,
+  } = React.useContext(TodoContext)
+
+    return (
+      <h1 className='TodoCounter'>
+        Haz completado <span>{completedTodos}</span> de <span>{totalTodos}</span> TODOS
+      </h1>
+    );
+  }
+
+  export { TodoCounter }
